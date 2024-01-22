0e80b8b8cdf0f0cffe88aa4704cd543c1a004bc8
hobbyscripterII <wruomma2@naver.com>
2024-01-22 17:30:26 +0900

    test : 게시판 mapper TDD 작성

--- src/test/java/com/baby/babycareproductsshop/CharEncodingConfig.java
+++ src/test/java/com/baby/babycareproductsshop/CharEncodingConfig.java
@@ -4,9 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-@Configuration  // 설정파일 어노테이션
+@Configuration  // 설정 파일 어노테이션
 public class CharEncodingConfig {
-
     @Bean
     public CharacterEncodingFilter characterEncodingFilter() {
         return new CharacterEncodingFilter("UTF-8", true);
