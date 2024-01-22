77d11c2793af3fab038d29a28eee719dbfcb676b
TH <kareme007@gmail.com>
2024-01-22 17:54:45 +0900

    feat : UserAddressControllerTest

--- src/test/java/com/baby/babycareproductsshop/user/UserAddressControllerTest.java
+++ src/test/java/com/baby/babycareproductsshop/user/UserAddressControllerTest.java
@@ -13,20 +13,23 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Import(CharEncodingConfig.class)
 @WebMvcTest(controllers = UserAddressController.class,
-        excludeAutoConfiguration = SecurityConfiguration.class,
+//        excludeAutoConfiguration = SecurityConfiguration.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JwtAuthenticationFilter.class))
 class UserAddressControllerTest {
     @Autowired
@@ -37,15 +40,21 @@ class UserAddressControllerTest {
     private ObjectMapper mapper;
 
     @Test
+    @WithMockUser
     void postUserAddress() throws Exception {
         ResVo result = new ResVo(1);
         given(service.postUserAddress(any())).willReturn(result);
         UserInsAddressDto dto = new UserInsAddressDto();
+        dto.setAddress("주소");
+        dto.setIuser(1);
+        dto.setAddressDetail("상세 주소");
+        dto.setZipCode("1234");
         String json = mapper.writeValueAsString(dto);
         mvc.perform(
                         MockMvcRequestBuilders.post("/api/user/address")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(json)
+                                .with(csrf())
                 )
                 .andExpect(status().isOk())
                 .andExpect(content().string(mapper.writeValueAsString(result)))
