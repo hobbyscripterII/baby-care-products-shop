5167d0c7688ee9ff44dde868e4016ddc403e37fd
TH <kareme007@gmail.com>
2024-01-24 12:37:59 +0900

    refactor : Order Part (TH)

--- src/main/java/com/baby/babycareproductsshop/order/OrderController.java
+++ src/main/java/com/baby/babycareproductsshop/order/OrderController.java
@@ -2,6 +2,7 @@ package com.baby.babycareproductsshop.order;
 
 import com.baby.babycareproductsshop.order.model.OrderInsDto;
 import com.baby.babycareproductsshop.order.model.OrderInsVo;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/order")
+@Tag(name = "주문 API", description = "주문 관련 파트")
 public class OrderController {
     private final OrderService service;
 
