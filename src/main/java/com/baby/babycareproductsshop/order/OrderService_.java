9b5715c08be8b42bb1b4f50d84c4791b35cda985
hobbyscripterII <wruomma2@naver.com>
2024-01-29 16:56:38 +0900

    refactor : 주문내역조회, 주문취소/반품내역조회 controller 통합

--- src/main/java/com/baby/babycareproductsshop/order/OrderService_.java
+++ src/main/java/com/baby/babycareproductsshop/order/OrderService_.java
@@ -1,21 +1,28 @@
 package com.baby.babycareproductsshop.order;
 
-import com.baby.babycareproductsshop.common.ProcessState;
-import com.baby.babycareproductsshop.order.model.OrderGetVo;
+import com.baby.babycareproductsshop.order.model.OrderGetDto;
+import com.baby.babycareproductsshop.order.model.OrderGetListVo;
 import com.baby.babycareproductsshop.security.AuthenticationFacade;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderService_ {
     private final OrderMapper_ mapper;
     private final AuthenticationFacade authenticationFacade;
 
-    public List<OrderGetVo> getOrder(int iorder) {
-        List<OrderGetVo> list = mapper.getOrder(iorder);
+    public List<OrderGetListVo> getOrder(OrderGetDto dto) {
+        List<OrderGetListVo> list = mapper.getOrder(dto);
+
         return list;
     }
 }
---
+++ src/main/java/com/baby/babycareproductsshop/order/model/OrderGetDto.java
@@ -0,0 +1,16 @@
+package com.baby.babycareproductsshop.order.model;
+
+import com.baby.babycareproductsshop.common.ProcessStateCodeToStringConverter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Data
+public class OrderGetDto {
+    @Schema(title = "주문 PK", description = "")
+    private int iorder;
+    @JsonIgnore
+    private int listFl;
+}
